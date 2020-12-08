8a52553da57342e12d1bb87d1b0559e2cd4358fc
mgusmano <mgusmano@salient.com>
2020-12-08 12:20:26 -0500

    add Ext

--- src/App.js
+++ src/App.js
@@ -3,23 +3,6 @@ import React, { useEffect, useState, useCallback } from "react";
 import AppPure from './AppPure';
 import useWindowDimensions from './WindowDimension'
 
-// function getWindowDimensions() {
-//   const { innerWidth: width, innerHeight: height } = window;
-//   return {width,height};
-// }
-
-// function useWindowDimensions() {
-//   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-//   useEffect(() => {
-//     function handleResize() {
-//       setWindowDimensions(getWindowDimensions());
-//     }
-//     window.addEventListener('resize', handleResize);
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
-//   return windowDimensions;
-// }
-
 const App = (props) => {
   const { height, width } = useWindowDimensions();
   const [headerheight, SetHeaderHeight] = useState(null)
--- src/ChildWindow.js
+++ src/ChildWindow.js
@@ -1,28 +1,59 @@
-import React, { useEffect  } from 'react'
-//const Ext = window['Ext']
+import React, { useRef, useEffect, useState } from 'react'
+import ReactResizeDetector from 'react-resize-detector';
+const Ext = window['Ext']
 
 const ChildWindow = (props) => {
-
+  const contentRef = useRef()
+  const [extitem, SetExtItem] = useState(null)
    
-    useEffect(() => {
+  useEffect(() => {
+    console.log('useEffect')
+    var w = {
+      xtype: 'panel',
+      xtitle: 'child',
+      layout: 'fit',
+      items: [
+        { xtype: 'button', text:'button'}
+      ]
+    }
+    w.width = '100%'
+    w.height = '100%'
+    console.log(w)
+    var theItem = Ext.create(w)
+    theItem.render(contentRef.current)
+    SetExtItem(theItem)
+  }, []);
 
-    }, []);
+  var l = props.layoutitem
   
-    var l = props.layoutitem
-    return (
-        <div 
-          style={{
-            fontSize:'11px',
-            width:'100%',
-            height:'100%',
-            border:'0px solid green',
-            overflow:'auto'
-          }} 
-        >
-          {props.item}-{props.layoutitem.widget.type}<br/>
-          x:{l.x},y:{l.y}<br/> w:{l.w}, h:{l.h}
-        </div> 
-    )
+  return (
+    //  <>
+    //  {extitem != null &&
+
+    <ReactResizeDetector handleWidth={true} handleHeight={true} onResize={(width, height)=> {
+      //console.log(theItem)
+      extitem.updateLayout();
+    }}>
+
+
+      <div 
+        ref={contentRef}
+        style={{
+          fontSize:'11px',
+          width:'100%',
+          height:'100%',
+          border:'0px solid green',
+          overflow:'auto'
+        }} 
+      >
+        {/* {props.item}-{props.layoutitem.widget.type}<br/>
+        x:{l.x},y:{l.y}<br/> w:{l.w}, h:{l.h} */}
+      </div> 
+
+      </ReactResizeDetector>
+//  }
+//  </>
+  )
 }
 
 export default ChildWindow
